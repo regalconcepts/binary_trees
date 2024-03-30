e688972b440a0b8070dd7c432a41199e52cbbc1e
regalconcepts <naboafo18@gmail.com>
2024-03-30 17:29:42 +0000

    updated block comments on binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -14,7 +14,7 @@
  *    * @parent: Pointer to the parent node
  *     * @left: Pointer to the left child node
  *      * @right: Pointer to the right child node
- *       */
+ */
 struct binary_tree_s
 {
 	int n;
@@ -31,7 +31,7 @@ typedef struct binary_tree_s heap_t;
  *  * struct levelorder_queue_s - Level order traversal queue.
  *   * @node: A node of a binary tree.
  *    * @next: The next node to traverse to in the binary tree.
- *     */
+ */
 typedef struct levelorder_queue_s
 {
 	binary_tree_t *node;
