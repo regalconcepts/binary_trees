68ce538e69bd65bb42b84d2c879fdaa1d4bc83b3
regalconcepts <naboafo18@gmail.com>
2024-03-30 17:15:24 +0000

    checked betty for 110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -22,7 +22,7 @@ int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
 }
 
 /**
- *  * binary_tree_is_bst - Checks if a binary tree is a valid binary search tree.
+ * binary_tree_is_bst - Checks if a binary tree is a valid binary search tree.
  *   * @tree: A pointer to the root node of the tree to check.
  *    *
  *     * Return: 1 if tree is a valid BST, and 0 otherwise
